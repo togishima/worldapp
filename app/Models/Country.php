89a390f60ef8e297ef1cfa65e1d3ee9c42e8ea98
Cloud User <centos@ip-172-31-51-215.ec2.internal>
2020-10-08 16:03:29 +0900

    返り値の記述修正

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,7 +9,8 @@ class Country extends Model
 {
     protected $table = "country";
 
-    public static function getCountryInfoDOM($c_id) {
+    public static function getCountryInfoDOM($c_id)
+    {
         try {
             $countryInfo = self::from('country_info')->where('Code2', $c_id)->get();
 
@@ -19,24 +20,24 @@ class Country extends Model
             $dl = [];
             $dl[] = '<dl class="country-info">';
             $dl[] = '<dt ' . $classTerm . '">Country Name:</dt>';
-            $dl[] = '<dd id="c-name"' . $classDef . '">' . $countryInfo[0]->Country_Name .'</dd>';
+            $dl[] = '<dd id="c-name"' . $classDef . '">' . $countryInfo[0]->Country_Name . '</dd>';
             $dl[] = '<dt ' . $classTerm . '">Government Form:</dt>';
-            $dl[] = '<dd id="govt-form"' . $classDef . '">' . $countryInfo[0]->GovernmentForm .'</dd>';
+            $dl[] = '<dd id="govt-form"' . $classDef . '">' . $countryInfo[0]->GovernmentForm . '</dd>';
             $dl[] = '<dt ' . $classTerm . '">Popolation:</dt>';
-            $dl[] = '<dd id="c-pop"' . $classDef . '">' . ($countryInfo[0]->Population / 1000000) .'M</dd>';
+            $dl[] = '<dd id="c-pop"' . $classDef . '">' . ($countryInfo[0]->Population / 1000000) . 'M</dd>';
             $dl[] = '<dt ' . $classTerm . '">GNP:</dt>';
-            $dl[] = '<dd id="c-gnp"' . $classDef . '">' . $countryInfo[0]->GNP .'</dd>';
+            $dl[] = '<dd id="c-gnp"' . $classDef . '">' . $countryInfo[0]->GNP . '</dd>';
             $dl[] = '<dt ' . $classTerm . '">Capital City:</dt>';
-            $dl[] = '<dd id="c-cap"' . $classDef . '">' . $countryInfo[0]->Capital .'</dd>';
+            $dl[] = '<dd id="c-cap"' . $classDef . '">' . $countryInfo[0]->Capital . '</dd>';
             $dl[] = '</dl>';
 
             return implode("", $dl);
         } catch (\Throwable $th) {
             throw $th;
         }
-        
     }
-    public static function getCountryInfo($c_id) {
-        return $countryInfo = self::from('country_info')->where('Code2', $c_id)->get();
-    } 
+    public static function getCountryInfo($c_id)
+    {
+        return self::from('country_info')->where('Code2', $c_id)->get();
+    }
 }
